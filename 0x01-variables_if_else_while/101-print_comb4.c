a085712a91b6af66dc41573766b05e44f51e7e93
Ketere <keterel8@gmail.com>
2023-08-14 21:50:52 +0000

    Advanced Task

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,6 +1,6 @@
 /*
  * File: 101-print_comb4.c
- * Auth: Brennan D Baraban
+ * Auth: Leonard Ketere
  */
 
 #include <stdio.h>
@@ -11,6 +11,7 @@
  *
  * Return: Always 0.
  */
+
 int main(void)
 {
 	int digit1, digit2, digit3;
