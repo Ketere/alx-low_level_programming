91f1bd115a813bfedbbbdcfc64a6fe57965372b7
Ketere <keterel8@gmail.com>
2023-08-15 22:13:17 +0000

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ *main - prints _putchar, followed by a new line
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+  _putcher('_');
+  _putcher('p');
+  _putcher('u');
+  _putcher('t');
+  _putcher('c');
+  _putcher('h');
+  _putcher('a');
+  _putcher('r');
+  _putcher('\n');
+
+  return (0);
+}
