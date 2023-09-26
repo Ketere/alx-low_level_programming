626c41b26c6ec0371f55c99cde00d4e89667fd57
Ketere <Keterel8@gmail.com>
2023-09-26 19:25:46 +0000

    Initial commit

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include "lists.h" // Include your header file that contains the listint_t structure and function prototype.
+#include "lists.h"
 
 /**
  * print_listint - Prints all the elements of a listint_t list.
@@ -9,14 +9,13 @@
  */
 size_t print_listint(const listint_t *h)
 {
-    size_t node_count = 0; // Initialize a variable to count nodes.
-
-    while (h != NULL) // Iterate through the list until the end (NULL) is reached.
+    size_t node_count = 0;
+    while (h != NULL)
     {
-        printf("%d\n", h->n); // Print the data of the current node.
-        h = h->next; // Move to the next node.
-        node_count++; // Increment the node count.
+        printf("%d\n", h->n);
+        h = h->next;
+        node_count++;
     }
 
-    return node_count; // Return the total number of nodes.
+    return node_count;
 }
