3ec5c10d0175dc5c9636c6597fdab91f947dcb8c
Ketere <Keterel8@gmail.com>
2023-09-27 02:56:49 +0000

    Adanced tasks

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,23 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "lists.h"
 
 /**
- * reverse_listint - prints a listint_t linked list.
- * @head: pointer to the list.
- * Return: number of nodes in the list.
- **/
+ * print_listint_safe - Prints a listint_t linked list safely.
+ * @head: A pointer to the head of the list.
+ *
+ * Return: The number of nodes in the list.
+ */
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t pichu = 0;
-	const listint_t *aux_node = head;
+    const listint_t *slow_ptr, *fast_ptr;
+    size_t count = 0;
 
-	if (!head)
-		exit(98);
+    slow_ptr = head;
+    fast_ptr = head;
 
-	while (aux_node)
-	{
-		printf("[%p] %i\n", (void *)aux_node, aux_node->n);
-		aux_node = aux_node->next;
-		pichu++;
-	}
-	return (pichu);
+    while (slow_ptr != NULL && fast_ptr != NULL && fast_ptr->next != NULL)
+    {
+        printf("[%p] %d\n", (void *)slow_ptr, slow_ptr->n);
+        count++;
+
+        slow_ptr = slow_ptr->next;
+        fast_ptr = fast_ptr->next->next;
+
+        if (slow_ptr == fast_ptr)
+        {
+            printf("[%p] %d\n", (void *)slow_ptr, slow_ptr->n);
+            count++;
+            break;
+        }
+    }
+
+    return count;
 }
